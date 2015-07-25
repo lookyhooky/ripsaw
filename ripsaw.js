34c4e149f8a45a8272eafc213368bd7d24f576c4
John Hooks <john@hookscrane.com>
2015-07-25 12:34:59 -0700

    added tests for chainable calls

---
+++ ripsaw.js
@@ -0,0 +1,118 @@
+/**
+ * ripsaw.js - An event system.
+ */
+
+var slice
+var toString
+var hasOwnProperty
+var hOP
+
+slice = Array.prototype.slice
+toString = Object.prototype.toString
+hasOwnProperty = hOP = Object.prototype.hasOwnProperty
+
+var ripsaw = {}
+
+ripsaw.on = function (name, cb, ctx) {
+  if ((typeof name !== 'string') &&
+      (toString.call(cb) !== '[object Function]')) {
+    return this
+  }
+  var channels = this._channels || (this._channels = {})
+  if (!hOP.call(channels, name)) { channels[name] = [] }
+  channels[name].push({cb: cb, ctx: ctx || this})
+  return this
+}
+
+ripsaw.off = function (name, cb) {
+  if ((typeof name !== 'string') &&
+      (toString.call(cb) !== '[object Function]')) {
+    return this
+  }
+  var i, len, channel, channels
+  channels = this._channels || (this._channels = {})
+  channel = (hOP.call(channels, name)) ? channels[name] : null
+  if (channel) {
+    for (i = 0, len = channel.length; i < len; i++) {
+      if (channel[i].cb === cb) {
+        channel.splice(i, 1)
+      }
+    }
+  }
+  return this
+}
+
+ripsaw.once = function (name, cb, ctx) {
+  if ((typeof name !== 'string') &&
+      (toString.call(cb) !== '[object Function]')) {
+    return this
+  }
+  var off = this.off.bind(this)
+  var modified = function () {
+    cb.apply(this, arguments)
+    off(name, modified)
+  }
+  this.on(name, modified, ctx)
+  return this
+}
+
+/**
+ * Create a copy of the channel array and iterate through it, applying each
+ * callback within the context and providing the additional arguments
+ * given to #trigger. The copy is created to avoid #once and #listenOnce from
+ * messing up the index when they remove themselves from a channel.
+ */
+ripsaw.trigger = function (name) {
+  if (typeof name !== 'string') {
+    return this
+  }
+  var i, len, args, copy, channel, channels
+  args = slice.call(arguments, 1)
+  channels = this._channels || (this._channels = {})
+  channel = (hOP.call(channels, name)) ? channels[name] : null
+  if (channel) {
+    copy = channel.slice()
+    for (i = 0, len = copy.length; i < len; i++) {
+      copy[i].cb.apply(copy[i].cxt, args)
+    }
+  }
+  return this
+}
+
+ripsaw.listen = function (obj, name, cb, cxt) {
+  if ((typeof name !== 'string') &&
+      (toString.call(cb) !== '[object Function]') &&
+      (toString.call(obj) !== '[object Object]' && !(hOP.call(obj, 'on')))) {
+    return this
+  }
+  obj.on(name, cb, cxt)
+  return this
+}
+
+ripsaw.stopListening = function (obj, name, cb) {
+  if ((typeof name !== 'string') &&
+      (toString.call(cb) !== '[object Function]') &&
+      (toString.call(obj) !== '[object Object]' && !(hOP.call(obj, 'off')))) {
+    return this
+  }
+  obj.off(name, cb)
+  return this
+}
+
+ripsaw.listenOnce = function (obj, name, cb, ctx) {
+  if ((typeof name !== 'string') &&
+      (toString.call(cb) !== '[object Function]') &&
+      (toString.call(obj) !== '[object Object]' &&
+       !(hOP.call(obj, 'listen')))) {
+    return this
+  }
+  var stopListening = this.stopListening.bind(this)
+  var modified = function () {
+    cb.apply(this, arguments)
+    stopListening(obj, name, modified)
+  }
+  this.listen(obj, name, modified, ctx)
+  return this
+}
+
+module.exports = ripsaw
--- test/ripsaw.js
+++ test/ripsaw.js
@@ -1,5 +1,5 @@
 var expect = require('chai').expect
-var ripsaw = require('../index')
+var ripsaw = require('../ripsaw')
 
 describe('Ripsaw', function () {
 
@@ -18,6 +18,38 @@ describe('Ripsaw', function () {
       expect(one._channels).to.have.property('foo')
         .with.length(1)
     })
+    it('should be chainable', function () {
+      expect(one.on('bar', callback)).to.equal(one)
+    })
+  })
+
+  describe('#once()', function () {
+    it('should excute then remove itself from the channel', function () {
+      var control = false
+
+      one.once('foo', function () { control = true })
+      one.trigger('foo')
+      expect(control).to.equal(true)
+
+      control = false
+      one.trigger('foo')
+      expect(control).to.equal(false)
+    })
+    it('should be chainable', function () {
+      expect(one.once('bar', callback)).to.equal(one)
+    })
+  })
+
+  describe('#off()', function () {
+    it('should remove an event from the channel', function () {
+      one.on('foo', callback)
+      expect(one._channels['foo']).to.have.length(1)
+      one.off('foo', callback)
+      expect(one._channels['foo']).to.have.length(0)
+    })
+    it('should be chainable', function () {
+      expect(one.off('bar', callback)).to.equal(one)
+    })
   })
 
   describe('#trigger()', function () {
@@ -35,32 +67,12 @@ describe('Ripsaw', function () {
       expect(control).to.have.length(2)
       expect(control[1]).to.equal('b')
     })
-  })
-
-  describe('#off()', function () {
-    it('should remove an event from the channel', function () {
-      one.on('foo', callback)
-      expect(one._channels['foo']).to.have.length(1)
-      one.off('foo', callback)
-      expect(one._channels['foo']).to.have.length(0)
+    it('should be chainable', function () {
+      expect(one.trigger('bar')).to.equal(one)
     })
   })
 
-  describe('#once()', function () {
-    it('should excute then remove itself from the channel', function () {
-      var control = false
-
-      one.once('foo', function () { control = true })
-      one.trigger('foo')
-      expect(control).to.equal(true)
-
-      control = false
-      one.trigger('foo')
-      expect(control).to.equal(false)
-    })
-  })
-
-  describe('#listen', function () {
+  describe('#listen()', function () {
     it('should add an event to a channel of another object', function () {
       one.listen(two, 'foo', callback)
       expect(two._channels['foo']).to.have.length(1)
@@ -71,19 +83,25 @@ describe('Ripsaw', function () {
       two.trigger('foo')
       expect(control).to.equal(true)
     })
+    it('should be chainable', function () {
+      expect(one.listen(two, 'bar', callback)).to.equal(one)
+    })
   })
 
-  describe('#stopListening', function () {
-    it('should remove an event from the listened to object', function () {
+  describe('#stopListening()', function () {
+    it('should remove an event from the target object', function () {
       one.listen(two, 'foo', callback)
       expect(two._channels['foo']).to.have.length(1)
       one.stopListening(two, 'foo', callback)
       expect(two._channels['foo']).to.have.length(0)
     })
+    it('should be chainable', function () {
+      expect(one.stopListening(two, 'bar', callback)).to.equal(one)
+    })
   })
 
-  describe('#listenOnce', function () {
-    it('should execute and then remove itself from the listened to object', function () {
+  describe('#listenOnce()', function () {
+    it('should execute and then remove itself from the target object', function () {
       var control = false
       var callback = function() { control = true }
       one.listenOnce(two, 'foo', callback)
@@ -92,6 +110,9 @@ describe('Ripsaw', function () {
       control = false
       two.trigger('foo')
       expect(control).to.equal(false)
+    })
+    it('should be chainable', function () {
+      expect(one.listenOnce(two, 'bar', callback)).to.equal(one)
     })
   })
 })
